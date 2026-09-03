03b38527dc5e388181e85900417a29a43208838a
YourNewGitHubUsername <njugunak.kevin@gmail.com>
2026-09-03 10:12:51 +0300

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
+
 import MostSearchedCars from "@/components/MostSearchedCars";
 import LatestCars from "@/components/LatestCars";
 import Footer from "@/components/Footer"; 
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#F4F6F9]">
-     <Navbar />
+     
       <Hero />
       <MostSearchedCars />
       <LatestCars />
